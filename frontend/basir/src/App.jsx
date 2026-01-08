9eb3974c712327bdbf4cdac212fd231f2495fd0b
shekh basir <shekhbasir9821273349@gmail.com>
2026-01-08 22:25:35 +0530

    navigatio bar add

--- frontend/basir/src/App.jsx
+++ frontend/basir/src/App.jsx
@@ -1,83 +1,19 @@
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { UserRound, LogOut } from "lucide-react";
+import Nav from "./allshared/Nav";
+import Home from "./allshared/Home";
+import Login from "./allshared/Login";
+import Signup from "./allshared/Signup";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 function App() {
-  const kamvail = true;
   return (
     <>
-      <div className="h-[60px] max-w-[full] bg-gray-200 flex justify-between pt-[10px]">
-        <div>
-          <h1 className="text-[25px] font-bold mr-[100px]">
-            Job <span className="text-red-600">khoj</span>
-          </h1>
-        </div>
-        <div className="flex  gap-5 text-[20px] mr-[20px]">
-          <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
-            Home
-          </h1>
-          <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
-            Jobs
-          </h1>
-          <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
-            Browse
-          </h1>
-          {!kamvail ? (
-            <div className="flex gap-5">
-              <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
-                Login
-              </h1>
-              <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
-                Logout
-              </h1>
-            </div>
-          ) : (
-            <Popover>
-              <PopoverTrigger className="  pb-[30px]">
-                <Avatar>
-                  <AvatarImage
-                    src="https://github.com/shadcn.png"
-                    className="cursor-pointer"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div className="flex gap-[10px]">
-                  <Avatar>
-                    <AvatarImage
-                      src="https://github.com/shadcn.png"
-                      className="cursor-pointer"
-                    />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                  <h1>Mern Stack Developer</h1>
-                </div>
-                <div className="text-gray-400 ml-[40px]">
-                  Lorem ipsum dolor sit amet.
-                </div>
-
-                <div className="flex  mt-[20px]">
-                  <UserRound />{" "}
-                  <Button variant="link" className="cursor-pointer">
-                    view Profile
-                  </Button>
-                </div>
-                <div className="flex mt-[10px]">
-                  <LogOut />
-                  <Button variant="link" className="cursor-pointer">
-                    LogOut
-                  </Button>
-                </div>
-              </PopoverContent>
-            </Popover>
-          )}
-        </div>
-      </div>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/basir/src/allshared/Nav.jsx
@@ -0,0 +1,86 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { UserRound, LogOut } from "lucide-react";
+
+import { Link } from "react-router-dom";
+function Nav() {
+  const kamvail = false;
+  return (
+    <>
+      <div className="h-[60px] max-w-[full] bg-gray-200 flex justify-between pt-[10px]">
+        <div>
+          <h1 className="text-[25px] font-bold mr-[100px]">
+            Job <span className="text-red-600">khoj</span>
+          </h1>
+        </div>
+        <div className="flex  gap-5 text-[20px] mr-[20px]">
+          <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
+            <Link to="/">Home</Link>
+          </h1>
+          <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
+            Jobs
+          </h1>
+          <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
+            Browse
+          </h1>
+          {!kamvail ? (
+            <div className="flex gap-5">
+              <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
+                <Link to="/login">Login</Link>
+              </h1>
+              <h1 className="h-[30px] w-[85px] bg-gray-400 text-black flex justify-center rounded-[10px] cursor-pointer ">
+                <Link to="/signup">Signup</Link>
+              </h1>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger className="  pb-[30px]">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    className="cursor-pointer"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent>
+                <div className="flex gap-[10px]">
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      className="cursor-pointer"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <h1>Mern Stack Developer</h1>
+                </div>
+                <div className="text-gray-400 ml-[40px]">
+                  Lorem ipsum dolor sit amet.
+                </div>
+
+                <div className="flex  mt-[20px]">
+                  <UserRound />{" "}
+                  <Button variant="link" className="cursor-pointer">
+                    view Profile
+                  </Button>
+                </div>
+                <div className="flex mt-[10px]">
+                  <LogOut />
+                  <Button variant="link" className="cursor-pointer">
+                    LogOut
+                  </Button>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+export default Nav;
